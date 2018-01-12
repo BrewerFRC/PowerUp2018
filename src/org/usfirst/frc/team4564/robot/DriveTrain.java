21934598e802b1e3bd5130df257221b48cf936ce
BrewerFRC <stevejacobs@iname.com>
2018-01-11 21:20:48 -0500

    Drive accel methods

--- src/org/usfirst/frc/team4564/robot/DriveTrain.java
+++ src/org/usfirst/frc/team4564/robot/DriveTrain.java
@@ -15,6 +15,12 @@ public class DriveTrain extends RobotDrive {
 	private Encoder encoderL, encoderR;
 	private Heading heading;
 	
+	double driveSpeed = 0;
+	double turnSpeed = 0;
+	double slideSpeed = 0;
+	double driveAccel = .08;
+	double turnAccel = .08;
+	
 	public DriveTrain() {
 		super(frontL, backL, frontR, backR);
 		
@@ -25,5 +31,47 @@ public class DriveTrain extends RobotDrive {
 	
 	public Heading getHeading() {
 		return this.heading;
+	}
+	 public double driveAccelCurve(double target, double driveAccel) {
+		 if (Math.abs(driveSpeed - target) > driveAccel) {
+	            if (driveSpeed > target) {
+	                driveSpeed = driveSpeed - driveAccel;
+	            } else {
+	                driveSpeed = driveSpeed + driveAccel;
+	            }
+	        } else {
+	            driveSpeed = target;
+	        }
+	        return driveSpeed;
+	 }
+	 
+	 public double turnAccelCurve(double target, double turnAccel) {
+		 if (Math.abs(turnSpeed - target) > turnAccel) {
+	    		if (turnSpeed > target) {
+	    			turnSpeed = turnSpeed - turnAccel;
+	    		} else {
+	    			turnSpeed = turnSpeed + turnAccel;
+	    		}
+	    	} else {
+	    		turnSpeed = target;
+	    	}
+	    return turnSpeed;
+	}
+	 
+	public void manualDrive(double fl, double bl, double fr, double br, double sl, double sr) {
+		frontL.set(fl);
+		backL.set(bl);
+		frontR.set(fr);
+		backR.set(br);
+	}
+	
+	public void setDrive(double drive, double turn, double slide) {
+		arcadeDrive(drive,turn);
+	}
+	
+	public void accelDrive(double drive, double turn, double slide) {
+		drive = driveAccelCurve(drive, driveAccel );
+		turn = turnAccelCurve(turn, turnAccel);
+		arcadeDrive(drive, turn);
 	}
 }
