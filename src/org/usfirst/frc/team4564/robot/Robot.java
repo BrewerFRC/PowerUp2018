de457dcafb26a8bbd33c73fdbbed83149a6cb97a
Qwertyness <mccoythedev@gmail.com>
2018-01-21 20:32:36 -0500

    Bug fixes and sashay drive.

--- src/org/usfirst/frc/team4564/robot/Robot.java
+++ src/org/usfirst/frc/team4564/robot/Robot.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team4564.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 
@@ -10,42 +11,66 @@ import edu.wpi.first.wpilibj.Timer;
  * Created January 2018
  * 
  * @author Brewer FIRST Robotics Team 4564
- * @author Evan McCoy
+ * @author Evan McCoy And Sam "Woodie" Woodward
  */
 @SuppressWarnings("deprecation")
 public class Robot extends SampleRobot {
 	private DriveTrain dt = new DriveTrain();
+	private static final double P = 0.075, I = 0, D = 0.08;
 	private Xbox j = new Xbox(0);
+	private String gameData;
 
 	public Robot() {
 		
 	}
-
+	
+	
+	
 	@Override
 	public void robotInit() {
 		
 	}
+	
+	@Override
+	public void disabled() {
+		while (isDisabled()) {
+			gameData = DriverStation.getInstance().getGameSpecificMessage();
+			Common.dashStr("Game Data", gameData);
+			/*char c = gameData.charAt(0);
+			if (c == 'R') {
+				AND = &&
+				OR = ||
+			}*/
+			if (gameData.length() == 3) {
+				Common.dashBool("Do You Have Game Data", true);
+			} else {
+				Common.dashBool("Do You Have Game Data" , false);
+			}
+		}
+	}
+	
 	
 	/**
 	 * Control logic for autonomous mode.
 	 */
 	@Override
 	public void autonomous() {
-		Path path = new Path();
-		path.addDriveStraight(36, 0, 0.65)
-			.addPowerTurn(90, 0.65)
-			.addDriveStraight(48, 90, 0.9)
-			.addPIDDrive(36, 0.4, 0.65, 0.075, 0, 0.08, false, "driveSlow")
-			.addPowerTurnOverlay(0, 0.65)
-			.addPIDDrive(36, 0.4, 0.8, 0.075, 0, 0.08, false, "driveScale");
-		
+		Path path1 = new Path();
+		path1.addDriveStraight(36, 0, 0.65)
+			 .addDriveStraight(200, 9, 0.65)
+			 .addDriveStraight(36, 0, 0.65);
 		while (isEnabled() && isAutonomous()) {
 			long time = Common.time();
 			
-			double[] power = path.getDrive();
+			double[] power = path1.getDrive();
+			
+			Common.dashNum("gyroAngle", DriveTrain.instance().getHeading().getAngle());
+			Common.dashNum("leftPower", power[0]);
+			Common.dashNum("rightPower", power[1]);
+			
 			DriveTrain.instance().tankDrive(power[0], power[1]);
 			
-			Timer.delay((1000.0/Constants.REFRESH_RATE - (Common.time() - time))/1000);
+			Timer.delay(Math.max(0, (1000.0/Constants.REFRESH_RATE - (Common.time() - time))/1000));
 		}
 	}
 
@@ -58,10 +83,11 @@ public class Robot extends SampleRobot {
 		Path path = new Path();
 		path.addDriveStraight(60, 0, 0.65)
 			.addPowerTurn(76, 0.65)
-			.addDriveStraight(72, 90, 0.65)
+			.addDriveStraight(72, 90, 0.9)
 			.addDriveStraight(36, 90, 0.65)
-			.addPowerTurn(0, 0.65)
-			.addPIDDrive(36, 0.4, 0.8, 0.075, 0, 0.08, true, "driveScale");
+			.addPowerTurn(12, 0.65)
+			/* distance, angle, minPower, maxPower, P, I, D, inverted, name */
+			.addPIDDrive(36, 0, 0.4, 0.8, P, I, D, true, "driveScale");
 		path.start();
 		while (isEnabled() && isOperatorControl()) {
 			long time = Common.time();
@@ -77,25 +103,5 @@ public class Robot extends SampleRobot {
 			}
 			Timer.delay((1000.0/Constants.REFRESH_RATE - (Common.time() - time))/1000);
 		}
-		/*while (isEnabled() && isOperatorControl()) {
-			long time = Common.time();
-			
-			Common.dashNum("encoderL", dt.getLeftCounts());
-			Common.dashNum("encoderR", dt.getRightCounts());
-			Common.dashNum("velL", dt.getLeftVelocity());
-			Common.dashNum("velR", dt.getRightVelocity());
-			dt.updatePIDs();
-			if (j.when("x")) {
-				dt.resetEncoders();
-			}
-			if (j.getPressed("y")) {
-				dt.pidVelDrive();
-			}
-			else {
-				dt.tankDrive(0, 0);
-			}
-			
-			Timer.delay((1000.0/Constants.REFRESH_RATE - (Common.time() - time))/1000);
-		}*/
 	}
 }
